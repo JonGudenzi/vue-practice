dc3ed8a030cf14b8304fefd87a1a68e4475cae9b
JonGudenzi <jongudenzi@gmail.com>
2021-05-01 16:06:00 -0400

    initial push - setup a radom user generator app in vue.js

---
+++ random-user-gen/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    
+    data() {
+        return {
+firstName: 'Jon',
+lastName: 'Doe',
+email: 'jon@gmail.com',
+gender: 'male',
+picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
+    methods: {
+        getUser() {
+            this.firstName = 'Sam'
+            this.lastName = 'Smith'
+            this.email = 'sam@gmail.com'
+            this.gender = 'female'
+            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        }
+    }
+});
+
+app.mount('#app')
